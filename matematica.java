df58cc10e52a14a38a7aa42b6bd24d329e79a8c5
jtmota1 <89613381+jtmota1@users.noreply.github.com>
2021-09-25 14:17:38 -0300

    Create matematica.java
    Aula Modulo IV Parte 2- Java na Prática
    Prof. Antônio Marcos.
    Disponívelem:<https://youtu.be/SFq92OjBwRo>. Acesso em Set de 2021.

---
+++ matematica.java
@@ -0,0 +1,21 @@
+package com.company;
+
+public class matematica {
+
+    public static void main(String[] args) {
+        System.out.println("Eleva 4 ao cubo: " + Math.pow(4,3));
+        System.out.println ("Raiz quadrada de 225: "+Math.sqrt(225));
+        System.out.println("Logarítimo de 128: " +Math.log(128));
+        System.out.println("Valor absoluto: "+Math.abs(-3));
+        System.out.println("Arredondar para cima: "+Math.ceil(3.5));
+        System.out.println("Arredondar para baixo: "+Math.floor(3.5));
+        System.out.println("Arredonda: "+Math.round(4.6));
+        System.out.println("Valor randômico entre 0 e 1: "+Math.random());
+        System.out.println("Maior valor: "+Math.max(10,2));
+        System.out.println("Menor valor: "+Math.min(20,2));
+
+
+
+
+    }
+}
