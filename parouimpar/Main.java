cdd4503fd8825f141c0173f652cacb5bc53221b4
jtmota1 <89613381+jtmota1@users.noreply.github.com>
2021-09-27 11:35:44 -0300

    Exercício de POO
    Exercício do curso Java POO

---
+++ parouimpar/Main.java
@@ -0,0 +1,11 @@
+package parouimpar;
+
+
+public class Main {
+
+    public static void main(String[] args) {
+        exibir j = new exibir();
+        j.exiba();
+
+    }
+}
---
+++ parouimpar/exibir.java
@@ -0,0 +1,16 @@
+package parouimpar;
+
+public class exibir {
+
+public void exiba () {
+    int contador = 1;
+        while(contador <=20)
+
+    {
+        if (contador%2 != 0) {
+            System.out.println(contador + "é ímpar");
+        }
+        contador++;
+    }
+    }
+    }
