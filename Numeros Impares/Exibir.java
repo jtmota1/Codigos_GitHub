ece75c527ad3810c3d23770fc2bb992bc9d1fcad
jtmota1 <89613381+jtmota1@users.noreply.github.com>
2021-09-27 18:36:08 -0300

    Modulo III Parte 2- Java na Prática
    Modulo III Parte 2- Java na Prática. Aula do Prof. Antônio Marcos, disponível em:<https://youtu.be/fzM3GFKBc04>. Acesso em 27 de Setembro de 2021.

---
+++ Numeros Impares/Exibir.java
@@ -0,0 +1,15 @@
+package parouimpar;
+
+public class Exibir {
+
+public void exiba () {
+    int contador = 1;
+        while(contador <=20)
+        {
+        if (contador%2 != 0) {
+            System.out.println(contador + "é ímpar");
+        }
+             contador++;
+            }
+        }
+    }
---
+++ Numeros Impares/Main.java
@@ -0,0 +1,10 @@
+package parouimpar;
+
+
+public class Main {
+
+    public static void main(String[] args) {
+        Exibir j = new Exibir();
+        j.exiba();
+    }
+}
